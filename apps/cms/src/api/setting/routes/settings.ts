07603fb8076de4366a378f8dfdada13286d2719d
jakubrauk <kubarauk@gmail.com>
2026-05-18 21:44:14 +0200

    fix(reviews): rename settings api dir to setting to match singularName

---
+++ apps/cms/src/api/setting/routes/settings.ts
@@ -0,0 +1,3 @@
+import { factories } from '@strapi/strapi';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default factories.createCoreRouter('api::setting.setting' as any);
